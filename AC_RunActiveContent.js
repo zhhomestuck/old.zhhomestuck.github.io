cfce74cc261a8d5bea701dac6fc152937cf9521d
leonw774 <33317554+leonw774@users.noreply.github.com>
2018-04-03 21:54:18 +0800

    Update AC_RunActiveContent.js

--- AC_RunActiveContent.js
+++ AC_RunActiveContent.js
@@ -3,7 +3,7 @@ function ControlVersion(){var e,a;try{a=new ActiveXObject("ShockwaveFlash.Shockw
 // Copyright 2018 Viz Media & Homestuck
 function vizFlashContentWrapper(){
 	if(DetectFlashVer(9,0,0)) {
-		arguments.src.replace(/\/flash/, "https://www.homestuck.com/flash")
+		arguments[9].replace(/\/flash/, "https://www.homestuck.com/flash")
 		AC_FL_RunContent.apply(null,arguments);
 	}
 	else{
