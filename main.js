e2e47854aa49b3a4b79ad96e3527c25a284dc3d8
leonw774 <leonw774@gmail.com>
2019-03-17 03:52:16 +0800

    Update 2019-03-17

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 var makeSpoilerLog = function() {
     var replaceSpoiler = document.getElementById("log-outer-outer");
     var replaceSpoilerName = "Pesterlog";
-    if ("name" in replaceSpoiler.attributes) replaceSpoilerName = replaceSpoiler.getAttribute("name");
+    if (replaceSpoiler !== null) {
+        if ("name" in replaceSpoiler.attributes) replaceSpoilerName = replaceSpoiler.getAttribute("name");
+    }
     if(replaceSpoiler) {
         replaceSpoiler.innerHTML = "<div class=\"log-outer\"><div><button type=\"button\" onmouseover=\"this.sv=this.style.backgroundColor;this.style.backgroundColor=\'#777777\';\" onmouseout=\"if(this.sv)this.style.backgroundColor=this.sv;else this.style.backgroundColor=\'\';\" onclick=\"this.parentNode.parentNode.childNodes[1].style.display=\'block\';this.parentNode.style.display=\'none\';return false;\" title=\"點擊以顯示\">Show " + replaceSpoilerName + "</button></div><div class=\"spoiler\" style=\"display:none;\"><div><button type=\"button\" onclick=\"this.parentNode.parentNode.parentNode.childNodes[0].style.display=\'block\';this.parentNode.parentNode.style.display=\'none\';return false;\" title=\"點擊以隱藏\">Hide " + replaceSpoilerName + "</button></div><div class=\"log-inner\">" + replaceSpoiler.innerHTML + "</div></div></div><!-- LOG END -->";
     }
@@ -14,7 +16,7 @@ var checkHTTPSWarning = function() {
     var has_interact_elemment = document.getElementsByTagName("embed").length != 0 || document.getElementsByTagName("object").length != 0;
     if(this_page_url){
         if(/https/.test(this_page_url) && has_interact_elemment){
-            var warning_text = "<br/><span>你使用了https連線 這可能會導致部分動畫檔案無法顯示 [<a onclick='this.parentNode.style.display=\"none\";' style='color:#0000ff;cursor:pointer;'>隱藏此訊息</a>]</span>";
+            var warning_text = "<span>你使用了https連線 這可能會導致部分動畫檔案無法顯示 [<a onclick='this.parentNode.style.display=\"none\";' style='color:#0000ff;cursor:pointer;'>隱藏此訊息</a>]</span>";
             var warning_node = document.createElement("div");
             warning_node.innerHTML = warning_text;
             warning_node.style.fontSize = "13px";
