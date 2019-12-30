dd8741c2eff47fbccbc845c7250b61f09d7beb4d
leonw774 <leonw774@gmail.com>
2019-12-30 20:50:26 +0800

    update 2019-12-30
    added more meta data in head
    deleted unnecessary id attrs
    add x2combo styles in main.css

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 // Make log
 var makeSpoilerLog = function() {
-    var replaceSpoiler = document.getElementById("log-outer-outer");
-    var replaceSpoilerName = "Pesterlog";
-    if (replaceSpoiler !== null) {
-        if ("name" in replaceSpoiler.attributes) replaceSpoilerName = replaceSpoiler.getAttribute("name");
+    let spoilerDOMs = [].slice.call(document.getElementsByClassName(&quot;log-outer-outer&quot;));
+    let spoilerDOMThatUseIdAttr = document.getElementById("log-outer-outer"); // beccause I fucked up
+    if (spoilerDOMThatUseIdAttr !== null) {
+        spoilerDOMs.push(spoilerDOMThatUseIdAttr)
     }
-    if(replaceSpoiler) {
-        replaceSpoiler.innerHTML = "<div class=\"log-outer\"><div><button type=\"button\" onmouseover=\"this.sv=this.style.backgroundColor;this.style.backgroundColor=\'#777777\';\" onmouseout=\"if(this.sv)this.style.backgroundColor=this.sv;else this.style.backgroundColor=\'\';\" onclick=\"this.parentNode.parentNode.childNodes[1].style.display=\'block\';this.parentNode.style.display=\'none\';return false;\" title=\"點擊以顯示\">Show " + replaceSpoilerName + "</button></div><div class=\"spoiler\" style=\"display:none;\"><div><button type=\"button\" onclick=\"this.parentNode.parentNode.parentNode.childNodes[0].style.display=\'block\';this.parentNode.parentNode.style.display=\'none\';return false;\" title=\"點擊以隱藏\">Hide " + replaceSpoilerName + "</button></div><div class=\"log-inner\">" + replaceSpoiler.innerHTML + "</div></div></div><!-- LOG END -->";
+    for (let i in spoilerDOMs) {
+        let spoilerName = "Pesterlog";
+        if ("name" in spoilerDOMs[i].attributes) {
+            spoilerName = spoilerDOMs[i].getAttribute("name");
+        }
+        spoilerDOMs[i].innerHTML = "<div class=\"log-outer\"><div><button type=\"button\" onmouseover=\"this.sv=this.style.backgroundColor;this.style.backgroundColor=\'#777777\';\" onmouseout=\"if(this.sv)this.style.backgroundColor=this.sv;else this.style.backgroundColor=\'\';\" onclick=\"this.parentNode.parentNode.childNodes[1].style.display=\'block\';this.parentNode.style.display=\'none\';return false;\" title=\"點擊以顯示\">Show " + spoilerName + "</button></div><div class=\"spoiler\" style=\"display:none;\"><div><button type=\"button\" onclick=\"this.parentNode.parentNode.parentNode.childNodes[0].style.display=\'block\';this.parentNode.parentNode.style.display=\'none\';return false;\" title=\"點擊以隱藏\">Hide " + spoilerName + "</button></div><div class=\"log-inner\">" + spoilerDOMs[i].innerHTML + "</div></div></div><!-- LOG END -->";
     }
 }
 
 // Warning for HTTPS connection
 var checkHTTPSWarning = function() {
-    var this_page_url = window.location.href;
-    var has_interact_elemment = document.getElementsByTagName("embed").length != 0 || document.getElementsByTagName("object").length != 0;
+    let this_page_url = window.location.href;
+    let has_interact_elemment = document.getElementsByTagName("embed").length != 0 || document.getElementsByTagName("object").length != 0;
     if(this_page_url){
         if(/https/.test(this_page_url) && has_interact_elemment){
-            var warning_text = "<span>由於某些瀏覽器不再支援Flash 部分動畫檔案可能無法顯示 請到官方網站觀看更新的版本 [<a onclick='this.parentNode.style.display=\"none\";' style='color:#0000ff;cursor:pointer;'>隱藏此訊息</a>]</span>";
-            var warning_node = document.createElement("div");
+            let warning_text = "<span>由於某些瀏覽器不再支援Flash 部分動畫檔案可能無法顯示 請到官方網站觀看更新的版本 [<a onclick='this.parentNode.style.display=\"none\";' style='color:#0000ff;cursor:pointer;'>隱藏此訊息</a>]</span>";
+            let warning_node = document.createElement("div");
             warning_node.innerHTML = warning_text;
             warning_node.style.fontSize = "12px";
             document.getElementById("pagehead").appendChild(warning_node);
