093e204356572b004e78c3141ca49f85a5344a78
leonw774 <33317554+leonw774@users.noreply.github.com>
2020-09-19 23:15:28 +0800

    update 2020-09-19

--- Viz_AC_RunActiveContent.js
+++ Viz_AC_RunActiveContent.js
@@ -14,7 +14,7 @@ function vizFlashContentWrapper(){
         var a=AC_GetArgs(arguments,".swf","movie",null,null),
             t='** 若要查看原始的呈現內容，請在<nobr>啟用Flash</nobr>的設備下觀看。 (<a href="/'+window.location.pathname.replace(/^\/+/g,"")+'?fl=1">顯示看看</a>)';
         if(a.params.youtubeid)
-            document.write('<div id="o_flash-container" style="position:relative;width:100%;height:0;padding:69.23076923076923% 0 0 0;"><iframe style="position:absolute;left:0;right:0;top:0;bottom:0;height:100%;width:100%;" src="https://www.youtube.com/embed/'+a.params.youtubeid+'" frameborder="0" allowfullscreen></iframe></div>');
+            document.write('<div id="o_flash-container"><iframe style="position:absolute;left:0;right:0;top:0;bottom:0;height:100%;width:100%;" src="https://www.youtube.com/embed/'+a.params.youtubeid+'" frameborder="0" allowfullscreen></iframe></div>');
         else if(a.params.altimgsrc){
             var n='<img src="'+a.params.altimgsrc+'">';a.params.altimghref&&(n='<a href="'+a.params.altimghref+'">'+n+"</a>"),document.write(n)}
         else 
