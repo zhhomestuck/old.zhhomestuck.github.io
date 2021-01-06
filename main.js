29c20f26b7cb63c6119018280316ffd64482e59f
leonw774 <33317554+leonw774@users.noreply.github.com>
2021-01-07 02:03:23 +0800

    add swf2js (testing)

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ var flashWarning = function() {
     let has_interact_elemment = document.getElementsByTagName("embed").length != 0 || document.getElementsByTagName("object").length != 0;
     if(this_page_url){
         if(has_interact_elemment){
-            let warning_text = "<span>由於Adobe Flash播放器已於2021年起停止支援，若此內容無法呈現，請到官方網頁觀看內容，或連繫通知管理員更新頁面。 [<a onclick='this.parentNode.style.display=\"none\";' style='color:#0000ff;cursor:pointer;'>隱藏此訊息</a>]</span>";
+            let warning_text = "<span>由於Adobe Flash播放器已於2021年起停止支援，若此內容無法呈現，請到官方網頁觀看內容，或連繫通知管理員更新頁面。 [<a onclick='this.parentNode.style.display=\"none\";' style='color:#0000ff;cursor:pointer;'>隱藏此訊息</a>] [<a onclick='loadswf2js();' style='color:#0000ff;cursor:pointer;'>測試swf2js導入</a>]</span>";
             let warning_node = document.createElement("div");
             warning_node.innerHTML = warning_text;
             warning_node.style.fontSize = "12px";
@@ -38,4 +38,36 @@ var flashWarning = function() {
 
 var linkOfficial = function() {
     document.getElementById("official-link").href = "https://www.homestuck.com/story/" + (parseInt(document.URL.substr(document.URL.indexOf('00')+2,4))-1900).toString();
+}
+
+var loadswf2js = function() {
+    var objArr = document.getElementsByTagName("object");
+    if (objArr.length == 1) {
+        if (objArr[0].type == "application/x-shockwave-flash") {
+            objArr[0].style.display = "none";
+            
+            var option = {};
+            option.tagId = "swf2js-container";
+            option.width = objArr[0].width;
+            option.height = objArr[0].height;
+            option.callback = (function() {
+                document.getElementById("swf2js-preloader").remove();
+            });
+            console.log(option);
+            
+            var swfurl = objArr[0].data;
+            
+            var preloading_img = document.createElement("img");
+            preloading_img.setAttribute("id", "swf2js-preloader");
+            preloading_img.setAttribute("src", "/assets/preloader.gif");
+            document.getElementsByClassName("pagebody")[0].insertBefore(preloading_img, objArr[0]);
+            
+            var container = document.createElement("div");
+            container.setAttribute("id", "swf2js-container");
+            document.getElementsByClassName("pagebody")[0].insertBefore(container, objArr[0]);
+            
+            swf2js.load(swfurl, option);
+        }
+    }
+    
 }
